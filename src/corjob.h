8020861fe9543cce7d1491969a7f2baea5c13240
Kanwarpal Brar <k.brar@carta.com>
2024-10-29 20:59:15 +0000

    Now handling coroutine main end without segfault.

--- src/corjob.h
+++ src/corjob.h
@@ -1,7 +1,6 @@
 #include "boost/context/continuation.hpp"
 #include <boost/context/fixedsize_stack.hpp>
 #include <iostream>
-
 namespace ctx=boost::context;
 
 int BASEID = 0;
@@ -15,11 +14,14 @@ class CorJob  {
 
   protected:
     ctx::continuation&& main(ctx::continuation&& sink) {
-      companion = nullptr;
+      // TODO: needs wrapper
       // override this method
       std::cout << "1" << std::endl;
       sink=sink.resume();
       std::cout << "2" << std::endl;
+
+      // invalidate parent's ptr to self on end
+      companion->companion=nullptr; // TODO: put in wrapper
       return std::move(sink);
     }
 
@@ -27,44 +29,45 @@ class CorJob  {
 
     // New Stack Companion CorJob
     template< typename StackAllocator >
-    CorJob(void * stk_ptr, std::size_t size, ctx::stack_context sctx, StackAllocator salloc): cont{} {
-        cont=ctx::callcc(
-          std::allocator_arg,
-          ctx::preallocated(stk_ptr, size, sctx),
-          salloc,
-          std::bind(&CorJob::main, this, std::placeholders::_1)
-          // std::bind(&CorJob::bootstrap, std::placeholders::_1, this)
-        ); // bootstrap needs access to the callcc stack-top object ptr
-        std::cout << "exited const" << std::endl;
-    }
+    CorJob(void * stk_ptr, std::size_t size, ctx::stack_context sctx, StackAllocator salloc, CorJob* parent_comp): cont{}, companion{parent_comp} {
+      cont=ctx::callcc(
+        std::allocator_arg,
+        ctx::preallocated(stk_ptr, size, sctx),
+        salloc,
+        std::bind(&CorJob::main, this, std::placeholders::_1)
+        // std::bind(&CorJob::bootstrap, std::placeholders::_1, this)
+      ); // bootstrap needs access to the callcc stack-top object ptr
+      std::cout << "exited const" << std::endl;
+  }
 
     void init_stack() {
-        // create stack allocator; StackAllocator is fixedsize_stack
-        ctx::fixedsize_stack salloc(4096);
+      // create stack allocator; StackAllocator is fixedsize_stack
+      ctx::fixedsize_stack salloc(4096);
 
-        // allocate stack space
-        ctx::stack_context sctx(salloc.allocate());
+      // allocate stack space
+      ctx::stack_context sctx(salloc.allocate());
 
-        // Move stack pointer to reserve space for control structure
-        void * stk_ptr=static_cast<char*>(sctx.sp)-sizeof(CorJob);
-        std::size_t size=sctx.size-sizeof(CorJob);
+      // Move stack pointer to reserve space for control structure
+      void * stk_ptr=static_cast<char*>(sctx.sp)-sizeof(CorJob);
+      std::size_t size=sctx.size-sizeof(CorJob);
 
-        // place new mirror CorJob on stack
-        companion=new(stk_ptr)CorJob(stk_ptr, size, sctx, salloc);
+      // place new mirror CorJob on stack, pass self as companion
+      companion=new(stk_ptr)CorJob(stk_ptr, size, sctx, salloc, this);
     }
 
   public:
 
     bool is_done() {
-        if (companion != nullptr) {
-            return companion->is_done();
-        }
-        return finished;
-        // return companion->is_done() ? companion != nullptr : finished;
+      // TODO: handle setting self to finished
+      if (!finished && companion != nullptr) {
+          return companion->is_done();
+      }
+      return finished;
+      // return companion->is_done() ? companion != nullptr : finished;
     }
 
     CorJob() {
-        init_stack();
+      init_stack();
     }
 
     void operator()() {
@@ -77,8 +80,6 @@ class CorJob  {
       }
       std::cout << "exited" << std::endl;
     }
-
-
 };
 
 
--- src/demo.cc
+++ src/demo.cc
@@ -4,7 +4,7 @@
 int main() {
     CorJob job;
     std::cout << "back to main" << std::endl;
-    std::cout << "isdone1: " << job.is_done() << std::endl;
+    // std::cout << "isdone1: " << job.is_done() << std::endl;
     job();
     std::cout << "isdone2: " << job.is_done() << std::endl;
 }
